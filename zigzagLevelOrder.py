aa8456fd844fcd7117f12e6fe47d67bcd988cb2b
YasubGetu <95662887+YasubGetu@users.noreply.github.com>
2022-08-26 15:34:11 +0300

    Create zigzagLevelOrder.py

---
+++ zigzagLevelOrder.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        ans = []
+        if not root:
+            return ans
+        queue = [root]
+        direction = 1
+        while queue:
+            level = []
+            for i in range(len(queue)):
+                curr = queue.pop(0)
+                level.append(curr.val)
+                if curr.left:
+                    queue.append(curr.left)
+                if curr.right:
+                    queue.append(curr.right)
+            ans.append(level[::direction])
+            direction *= -1
+        return ans    
